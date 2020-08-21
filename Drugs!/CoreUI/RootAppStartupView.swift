4af65ebf90993415f809eff5308e2f5c91227b26
Ivan Lugo <ivan.lugo@avast.com>
2020-08-21 16:42:23 -0700

    - Cleaning up and writing some comments

--- Drugs!/CoreUI/RootAppStartupView.swift
+++ Drugs!/CoreUI/RootAppStartupView.swift
@@ -1,18 +1,36 @@
 import SwiftUI
 import Combine
 
-enum TabTags {
+enum RootScreenTabTag {
     case home, notifications
 }
 
 struct RootAppStartupView: View {
 
-    @State var selectionTag: TabTags = .home
+    @State var selectionTag: RootScreenTabTag = .home
 
     var body: some View {
-        TabView(selection: $selectionTag) {
-            homeViewOrEmpty
-            infoViewOrEmpty
+        /** As of:
+         * XCode 12.0 beta 4 (12A8179i)
+         * iOS 14.0 Beta 5 (18A5351d), and iOS 14.0 'default simulator'
+         *
+         * TabView automatically recreates and keeps alive tabs that aren't selected.
+         * This means any subscriptions and lifecyle events are kept around. This is
+         * best tested with a .viewAppeared(). So, there's a great little hack:
+         * Use selection state to simply not show the view, forcing SwiftUI to recreate
+         * the hierarchy entirely with a new view, seemingly defeating whatever optimization or
+         * retention is occuring. How long this will work is completely unknown.
+         */
+        if #available(iOS 14.0, *) {
+            TabView(selection: $selectionTag) {
+                homeViewOrEmpty
+                infoViewOrEmpty
+            }
+        } else {
+            TabView(selection: $selectionTag) {
+                homeView.asHomeTab
+                notificationsView.asNotificationsTab
+            }
         }
     }
 
@@ -21,31 +39,19 @@ struct RootAppStartupView: View {
             if selectionTag == .home {
                 homeView
             } else {
-                VStack {
-                    ActivityIndicator(isAnimating: .constant(true), style: .large)
-                    Text("Loading home...")
-                }
+                loadingStack("Loading home...")
             }
-        }.tabItem {
-            Image(systemName: "list.bullet")
-            Text("Medicine Log")
-        }.tag(TabTags.home)
+        }.asHomeTab
     }
 
     private var infoViewOrEmpty: some View {
         Group {
             if selectionTag == .notifications {
-                NotificationInfoView()
+                notificationsView
             } else {
-                VStack {
-                    ActivityIndicator(isAnimating: .constant(true), style: .large)
-                    Text("Loading reminders...")
-                }
+                loadingStack("Loading reminders...")
             }
-        }.tabItem {
-            Image(systemName: "calendar.circle.fill")
-            Text("Reminders")
-        }.tag(TabTags.notifications)
+        }.asNotificationsTab
     }
 
     private var homeView: some View {
@@ -56,6 +62,45 @@ struct RootAppStartupView: View {
             )
         }
         .navigationViewStyle(StackNavigationViewStyle())
+    }
+
+    private var notificationsView: some View {
+        NotificationInfoView()
+    }
+
+    private func loadingStack(_ text: String) -> some View {
+        VStack {
+            ActivityIndicator(isAnimating: .constant(true), style: .large)
+            Text(text)
+        }
+    }
+}
+
+extension View {
+    var asHomeTab: some View {
+        return modifier(TagModifier(tag: .home))
+    }
+
+    var asNotificationsTab: some View {
+        return modifier(TagModifier(tag: .notifications))
+    }
+}
+
+struct TagModifier: ViewModifier {
+    let tag: RootScreenTabTag
+    func body(content: Content) -> some View {
+        switch tag {
+        case .home:
+            return content.tabItem {
+                Image(systemName: "list.bullet")
+                Text("Medicine Log")
+            }.tag(RootScreenTabTag.home)
+        case .notifications:
+            return content.tabItem {
+                Image(systemName: "calendar.circle.fill")
+                Text("Reminders")
+            }.tag(RootScreenTabTag.notifications)
+        }
     }
 }
 
