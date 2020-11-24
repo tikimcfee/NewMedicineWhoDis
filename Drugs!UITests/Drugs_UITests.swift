984eb453f59251753a9e431a1c4f2be283e2c97e
Ivan Lugo <ivan.lugo@avast.com>
2020-11-23 21:06:24 -0800

    Testing and stuff

--- Drugs!UITests/Drugs_UITests.swift
+++ Drugs!UITests/Drugs_UITests.swift
@@ -8,70 +8,6 @@
 
 import XCTest
 
-class Drugs_UITests: XCTestCase {
-
-    override func setUp() {
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-    }
-
-    override func tearDown() {
-
-    }
-
-//    func testRootScreen() {
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        NumberPad.allCases.forEach { number in
-//            XCTAssert(app.buttons[number.rawValue].exists, "Number pad is broken: \(number)")
-//        }
-//    }
-
-    func testCreateAndSave() {
-        let app = XCUIApplication()
-
-        func assertTableData() {
-            let mainTable = app.tables[HomeButtons.entryCellList.rawValue]
-            XCTAssert(mainTable.exists, "Main list wasn't found")
-
-            // TODO: Ask someone why child labels get 'swallowed up' and they can't be found.
-            // This is happening with a Button that has a 'View' with two 'Text' children
-            let entryButtons = app.buttons[HomeButtons.entryCellButton.rawValue]
-            let firstButton = entryButtons.firstMatch.label.lowercased()
-            let containsDrugs =
-                firstButton.contains(DrugList.Dramamine.rawValue.lowercased())
-                && firstButton.contains(DrugList.Excedrin.rawValue.lowercased())
-                && firstButton.contains(DrugList.Ibuprofen.rawValue.lowercased())
-            XCTAssert(containsDrugs, "Drugs are missing from the entry.")
-        }
-
-        // Set first app launch options
-        app.launchArguments = [
-            AppTestArguments.enableTestConfiguration.rawValue,
-            AppTestArguments.clearEntriesOnLaunch.rawValue,
-        ]
-        app.launch()
-
-        // Tap some medicine, then save
-        app.tap(.Dramamine)
-        app.tap(.two)
-        app.tap(.Excedrin)
-        app.tap(.four)
-        app.tap(.Ibuprofen)
-        app.tap(.six)
-        app.tap(.saveEntry)
-        sleep(1)
-        assertTableData()
-
-        // Clear launch args, terminate, and restart
-        app.launchArguments = []
-        app.terminate()
-        app.launch()
-        assertTableData()
-    }
-}
-
 extension XCUIApplication {
     func tap(_ number: NumberPad) {
         buttons[number.rawValue].tap()
@@ -85,5 +21,90 @@ extension XCUIApplication {
         buttons[button.rawValue].tap()
     }
 }
+
+extension Drugs_UITests {
+    func checkFirstCellInTableFor(drugs: [DrugList]) {
+        let mainTable = app.tables[HomeButtons.entryCellList.rawValue]
+        XCTAssert(mainTable.exists, "Main list wasn't found")
+
+        // TODO: Ask someone why child labels get 'swallowed up' and they can't be found.
+        // This is happening with a Button that has a 'View' with two 'Text' children
+        let entryButtons = app.buttons[HomeButtons.entryCellButton.rawValue]
+        let firstEntryButtonText = entryButtons.firstMatch.label.lowercased()
+        let allDrugsInLabel = drugs.allSatisfy { firstEntryButtonText.contains($0.rawValue.lowercased()) }
+        XCTAssert(allDrugsInLabel, "Drugs are missing from the entry.")
+    }
+}
+
+
+class Drugs_UITests: XCTestCase {
+
+    // The app is created, not launched. Do this in each test.
+    // This keeps helper functions from needing app params.
+    var app: XCUIApplication!
+
+    override func setUp() {
+        continueAfterFailure = false
+        app = XCUIApplication()
+    }
+
+    override func tearDown() {
+
+    }
+
+    func testCreateAndSave() {
+        app.launchArguments = [
+            AppTestArguments.enableTestConfiguration.rawValue,
+            AppTestArguments.clearEntriesOnLaunch.rawValue,
+        ]
+        app.launch()
+
+        // Tap some medicine, then save
+        let testDrugs: [DrugList] = [.Dramamine, .Excedrin, .Ibuprofen]
+        app.tap(.Dramamine)
+        app.tap(.two)
+        app.tap(.Excedrin)
+        app.tap(.four)
+        app.tap(.Ibuprofen)
+        app.tap(.six)
+        app.tap(.saveEntry)
+        sleep(1)
+        checkFirstCellInTableFor(drugs: testDrugs)
+
+        // Check same drugs are in the list
+        app.launchArguments = []
+        app.terminate()
+        app.launch()
+        checkFirstCellInTableFor(drugs: testDrugs)
+    }
+
+    func destroyTheAppWithLotsOfData() {
+        app.launchArguments = [
+            AppTestArguments.enableTestConfiguration.rawValue,
+            AppTestArguments.clearEntriesOnLaunch.rawValue,
+        ]
+        app.launch()
+
+        // Tap some medicine, then save
+        let testDrugs: [DrugList] = [.Dramamine, .Excedrin, .Ibuprofen]
+        app.tap(.Dramamine)
+        app.tap(.two)
+        app.tap(.Excedrin)
+        app.tap(.four)
+        app.tap(.Ibuprofen)
+        app.tap(.six)
+        app.tap(.saveEntry)
+        sleep(1)
+        checkFirstCellInTableFor(drugs: testDrugs)
+
+        // Check same drugs are in the list
+        app.launchArguments = []
+        app.terminate()
+        app.launch()
+        checkFirstCellInTableFor(drugs: testDrugs)
+    }
+}
+
+
 
 
