4de43475ae305735a37afb7ed1ed28aed5c3addb
Ivan Lugo <ivan.lugo@knights.ucf.edu>
2022-02-11 17:57:15 -0800

    - Add and edit works.. I think?

---
+++ Drugs!/CoreLogic/Persistence/RealmFiles/RealmPersistenceManager.swift
@@ -0,0 +1,188 @@
+//
+//  RealmPersistenceManager.swift
+//  Drugs!
+//
+//  Created by Ivan Lugo on 2/11/22.
+//  Copyright © 2022 Ivan Lugo. All rights reserved.
+//
+
+import Foundation
+import Realm
+import Combine
+
+enum RealmPersistenceError: Error {
+    case invalidIndex(Int)
+}
+
+class RealmPersistenceManager: ObservableObject, PersistenceManager {
+    private let manager: EntryLogRealmManager
+    private let tranformer: RealmPersistenceStateTransformer
+    private let migrater = V1Migrator()
+    
+    init(manager: EntryLogRealmManager = DefaultRealmManager()) {
+        self.manager = manager
+        self.tranformer = RealmPersistenceStateTransformer(manager: manager)
+    }
+    
+    var appDataStream: AnyPublisher<ApplicationData, Never> {
+        tranformer.$appData.eraseToAnyPublisher()
+    }
+    
+    func checkAndCompleteMigrations(_ sourceFlatFile: FilePersistenceManager) throws {
+        manager.access { realm in
+            try migrater.migrate(manager: sourceFlatFile, into: realm)
+        }
+    }
+    
+    func getEntry(with id: String) -> MedicineEntry? {
+        try? manager.loadEntryLogRealm()
+            .object(ofType: RLM_MedicineEntry.self, forPrimaryKey: id)
+            .map { model in migrater.toV1Entry(model) }
+    }
+    
+    func addEntry(medicineEntry: MedicineEntry, _ handler: @escaping ManagerCallback) {
+        manager.access { realm in
+            do {
+                try realm.write {
+                    realm.add(migrater.fromV1Entry(medicineEntry), update: .all)
+                    handler(.success(()))
+                }
+            } catch {
+                handler(.failure(error))
+            }
+        }
+    }
+    
+    func removeEntry(index: Int, _ handler: @escaping ManagerCallback) {
+        manager.access { realm in
+            do {
+                let objects = realm.objects(RLM_MedicineEntry.self)
+                guard objects.count > index else { throw RealmPersistenceError.invalidIndex(index) }
+                let toRemove = objects[index]
+                try realm.write {
+                    realm.delete(toRemove)
+                    handler(.success(()))
+                }
+            } catch {
+                handler(.failure(error))
+            }
+        }
+    }
+    
+    struct UpdateEntryError: Error {
+        var updated: MedicineEntry
+        var reason = "undefined"
+    }
+    
+    func updateEntry(updatedEntry: MedicineEntry, _ handler: @escaping ManagerCallback) {
+        manager.access { realm in
+            do {
+                try realm.write {
+//                    guard let toUpdate = realm.object(ofType: RLM_MedicineEntry.self, forPrimaryKey: updatedEntry.id) else {
+//                        throw UpdateEntryError(updated: updatedEntry, reason: "Missing target entry")
+//                    }
+                    let updatedCopy = migrater.fromV1Entry(updatedEntry)
+                    realm.add(updatedCopy, update: .all)
+                    handler(.success(()))
+                }
+            } catch {
+                handler(.failure(error))
+            }
+        }
+    }
+    
+    struct UpdateError: Error {
+        var original: Drug
+        var updated: Drug
+        var reason = "undefined"
+    }
+    
+    func updateDrug(originalDrug: Drug, updatedDrug: Drug, _ handler: @escaping ManagerCallback) {
+        manager.access { realm in
+            do {
+                guard let availableDrugList = RLM_AvailableDrugList.defaultFrom(realm) else {
+                    throw UpdateError(original: originalDrug, updated: updatedDrug, reason: "Missing root drug list")
+                }
+                guard let drugToUpdate = availableDrugList.drugs.first(where: { $0.id == updatedDrug.id }) else {
+                    throw UpdateError(original: originalDrug, updated: updatedDrug, reason: "Missing drug in realm list")
+                }
+                try realm.write {
+                    drugToUpdate.hourlyDoseTime = updatedDrug.hourlyDoseTime
+                    drugToUpdate.ingredients.removeAll()
+                    drugToUpdate.ingredients.append(objectsIn: updatedDrug.ingredients.map(migrater.fromV1Ingredient))
+                    handler(.success(()))
+                }
+            } catch let error as UpdateError {
+                log { Event(error.reason, .error) }
+                handler(.failure(error))
+            } catch {
+                log { Event("\(error)", .error) }
+                handler(.failure(error))
+            }
+        }
+    }
+    
+    struct AddError: Error {
+        var new: Drug
+        var reason = "undefined"
+    }
+    
+    func addDrug(newDrug: Drug, _ handler: @escaping ManagerCallback) {
+        manager.access { realm in
+            do {
+                guard let availableDrugList = RLM_AvailableDrugList.defaultFrom(realm) else {
+                    throw AddError(new: newDrug, reason: "Missing root drug list")
+                }
+                guard availableDrugList.drugs.first(where: { $0.id == newDrug.id} ) == nil else {
+                    throw AddError(new: newDrug, reason: "ID already exists")
+                }
+                let newRealmDrug = migrater.fromV1drug(newDrug)
+                try realm.write {
+                    availableDrugList.drugs.append(newRealmDrug)
+                    handler(.success(()))
+                }
+            } catch {
+                handler(.failure(error))
+            }
+        }
+    }
+    
+    struct RemoveError: Error {
+        var remove: Drug
+        var reason = "undefined"
+    }
+    
+    func removeDrug(drugToRemove: Drug, _ handler: @escaping ManagerCallback) {
+        manager.access { realm in
+            do {
+                guard let availableDrugList = RLM_AvailableDrugList.defaultFrom(realm) else {
+                    throw RemoveError(remove: drugToRemove, reason: "Missing root drug list")
+                }
+                guard let index = availableDrugList.drugs.firstIndex(where: { $0.id == drugToRemove.id} ) else {
+                    throw RemoveError(remove: drugToRemove, reason: "ID missing")
+                }
+                try realm.write {
+                    availableDrugList.drugs.remove(at: index)
+                    handler(.success(()))
+                }
+            } catch {
+                handler(.failure(error))
+            }
+        }
+    }
+    
+#if DEBUG
+    func removeAllData() {
+        manager.access { realm in
+            try? realm.write {
+                let all = realm.objects(RLM_MedicineEntry.self)
+                realm.delete(all)
+            }
+        }
+    }
+    
+    func __internalAppData() -> ApplicationData {
+        return tranformer.appData
+    }
+#endif
+}
---
+++ Drugs!/CoreLogic/Persistence/RealmFiles/RealmPersistenceStateTransformer.swift
@@ -0,0 +1,72 @@
+//
+//  RealmPersistenceStateTransformer.swift
+//  Drugs!
+//
+//  Created by Ivan Lugo on 2/11/22.
+//  Copyright © 2022 Ivan Lugo. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import Combine
+
+class RealmPersistenceStateTransformer {
+    @Published var appData: ApplicationData = ApplicationData()
+    
+    private let manager: EntryLogRealmManager
+    private var entriesToken: NotificationToken?
+    private var drugsToken: NotificationToken?
+    
+    init(manager: EntryLogRealmManager) {
+        self.manager = manager
+        try? setupObservations()
+    }
+    
+    deinit {
+        entriesToken?.invalidate()
+        drugsToken?.invalidate()
+    }
+    
+    func doMigrations() throws {
+        manager.access { realm in
+            
+        }
+    }
+    
+    func setupObservations() throws {
+        let realm = try manager.loadEntryLogRealm()
+        entriesToken = realm
+            .objects(RLM_MedicineEntry.self)
+            .sorted(by: \.date, ascending: false)
+            .observe { [weak self] change in
+                switch change {
+                case let .initial(results):
+                    log { Event("Initial entry list loaded") }
+                    self?.appData.mainEntryList = results.map { V1Migrator().toV1Entry($0) }
+                case let .update(results, _, _, _):
+                    log { Event("Entry list updated") }
+                    self?.appData.mainEntryList = results.map { V1Migrator().toV1Entry($0) }
+                case let .error(error):
+                    log { Event("Failed to observe, \(error.localizedDescription)", .error)}
+                }
+            }
+        drugsToken = RLM_AvailableDrugList
+            .defaultListFrom(realm)
+            .observe { [weak self] change in
+                switch change {
+                case let .initial(results):
+                    log { Event("Initial drug list loaded") }
+                    if let first = results.first {
+                        self?.appData.availableDrugList = V1Migrator().toV1DrugList(first)
+                    }
+                case let .update(results, _, _, _):
+                    log { Event("Drug list updated") }
+                    if let first = results.first {
+                        self?.appData.availableDrugList = V1Migrator().toV1DrugList(first)
+                    }
+                case let .error(error):
+                    log { Event("Failed to observe, \(error.localizedDescription)", .error) }
+                }
+            }
+    }
+}
